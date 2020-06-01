9c19be089af35a8d57d5516b5eb1f67e1502e290
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-06-01 14:03:01 -0500

    can add todos and toggle completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
 import React from 'react';
 
+import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
+
+const todos = [
+  {
+    task: "mow lawn", 
+    id: 1234,
+    completed: false, 
+  }
+]; 
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state = {
+      todos: todos,
+    }
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  addTodo = todoTask => {
+    const newTodo = {
+      task: todoTask, 
+      id: Date.now(), 
+      completed: false, 
+    }
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    })
+  }
+
+  toggleCompleted = todoId => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === todoId) {
+          return {
+            ...todo, completed: !todo.completed
+          }
+        } else {
+          return todo; 
+        }
+      })
+    })
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <div className="header">
+          <h2>Welcome to your Todo App!</h2>
+          <TodoForm
+            addTodo={this.addTodo}
+          />
+        </div>
+        <TodoList
+          todos={this.state.todos}
+          toggleCompleted={this.toggleCompleted}
+        />
       </div>
     );
   }
